e02c7f093d70b6a01bcf1cfccc7c34bbe58afd70
Paul Elliott <paul@codingfrontier.com>
2017-07-29 11:51:52 -0400

    Remove rogue users table from db/schema

--- db/schema.rb
+++ db/schema.rb
@@ -23,11 +23,4 @@ ActiveRecord::Schema.define(version: 20170726213333) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end

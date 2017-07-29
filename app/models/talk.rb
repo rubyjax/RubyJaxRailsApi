e864e54e8677dcfb9cdf567dec39c951bc51f850
Ryan Murphy <murjax@gmail.com>
2017-07-29 14:09:18 -0400

    Only validate word length if fields are present

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -1,5 +1,13 @@
 class Talk < ApplicationRecord
   validates_presence_of :title, :category, :description
-  validates_with TitleLengthValidator
-  validates_with DescriptionLengthValidator
+  validates_with TitleLengthValidator, if: :title_present?
+  validates_with DescriptionLengthValidator, if: :description_present?
+
+  def title_present?
+    title.present?
+  end
+
+  def description_present?
+    description.present?
+  end
 end
